fdb258c2c26d001414d4e58635676f3bb15c352b
John Prabu A <johnprabu0702@gmail.com>
2024-11-23 22:31:19 +0530

    supplier/orders page temporary navigation button added

--- src/components/supplier/index.jsx
+++ src/components/supplier/index.jsx
@@ -1,10 +1,19 @@
+import { useNavigate } from "react-router-dom";
+
 function SupplierUI() {
-    return (
-      <>
+  const navigate = useNavigate();
+  return (
+    <>
       Supplier UI here
-      </>
-    );
-  }
-  
-  export default SupplierUI;
-  
+      <br/>
+      <button
+        className="bg-blue-500 text-white font-semibold px-8 py-4 rounded-lg shadow-lg hover:bg-blue-400 transition-all w-[200px] text-center m-4"
+        onClick={() => navigate("orders")}
+      >
+        Orders
+      </button>
+    </>
+  );
+}
+
+export default SupplierUI;
