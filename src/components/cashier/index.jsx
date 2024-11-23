c69e46f7b7387be1ded33e13ef5ac2af6ea870fb
John Prabu A <johnprabu0702@gmail.com>
2024-11-23 22:30:52 +0530

    casher/orders page navigation temporary button added

--- src/components/cashier/index.jsx
+++ src/components/cashier/index.jsx
@@ -1,8 +1,18 @@
+import { useNavigate } from "react-router-dom";
+
 function CashierUI() {
+    const navigate = useNavigate();
     return (
       <>
       Cashier UI here
-      </>
+      <br/>
+      <button
+        className="bg-blue-500 text-white font-semibold px-8 py-4 rounded-lg shadow-lg hover:bg-blue-400 transition-all w-[200px] text-center m-4"
+        onClick={() => navigate("orders")}
+      >
+        Orders
+      </button>
+    </>
     );
   }
   
