7e5023fcf35d4322738e70d12113a5f151e64eb8
KrupaJanani <enkrupajanani2004@gmail.com>
2025-01-12 12:20:47 +0530

    Navbar & Dashboard redone

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,10 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AdminUI from "./components/admin";
+import AdminDashBoard from "./components/admin/dashboard";
+import CashierDashBoard from "./components/cashier/dashboard";
+import SupplierDashBoard from "./components/supplier/dashboard";
 import SupplierUI from "./components/supplier";
 import CashierUI from "./components/cashier";
 import SupplierOrders from "./components/supplier/orders";
 import CashierOrders from "./components/cashier/orders";
 import App from "./App"; 
+import Navbar from "./components/Navbar";
 
 function RoutesComponent() {
   return (
@@ -16,6 +20,11 @@ function RoutesComponent() {
         <Route path="/cashier" element={<CashierUI />} />
         <Route path="/cashier/orders" element={<CashierOrders />} />
         <Route path="/supplier/orders" element={<SupplierOrders />} />
+
+        {/* Dashboard pages with Navbar */}
+        <Route path="/admin/dashboard/:u_id" element={<><Navbar /><AdminDashBoard /></>} />
+        <Route path="/cashier/dashboard/:u_id" element={<><Navbar /><CashierDashBoard /></>} />
+        <Route path="/supplier/dashboard/:u_id" element={<><Navbar /><SupplierDashBoard /></>} />
       </Routes>
     </Router>
   );
